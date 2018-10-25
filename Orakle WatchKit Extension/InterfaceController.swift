9e98d9cac77a154fc6e7e63ab8a9c56bc6561fb0
Nudpakun Leechaikul <nudpakun.l@om4you.com>
2018-10-25 23:20:43 +0700

    Initial Commit

---
+++ Orakle WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Orakle WatchKit Extension
+//
+//  Created by Nudpakun Leechaikul on 25/10/2561 BE.
+//  Copyright © 2561 OM FOR YOU Co., Ltd. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Orakle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Orakle
+//
+//  Created by Nudpakun Leechaikul on 25/10/2561 BE.
+//  Copyright © 2561 OM FOR YOU Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
